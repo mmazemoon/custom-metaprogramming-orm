df4fc84de4c53b16e9f1da20330cf66468c13c7d
Mark Azemoon <mmazemoon@gmail.com>
2015-05-08 11:31:19 -0700

    Implement a ::my_attr_accessor macro which operates exactly like the real attr_accessor: defines setter/getter methods

--- lib/00_attr_accessor_object.rb
+++ lib/00_attr_accessor_object.rb
@@ -1,14 +1,30 @@
 class AttrAccessorObject
+
   def self.my_attr_accessor(*names)
     names.each do |name|
+      # getter
       define_method(name) do
-        name
+         instance_variable_get("@#{name}".to_sym)
       end
-
-      define_method(name=) do
-        name = name
+      # setter
+      define_method("#{name}=".to_sym) do |arg|
+        instance_variable_set("@#{name}".to_sym, arg)
       end
 
     end
   end
+
+
+
 end
+
+
+# class#define_method(symbol) { block } → symbol
+
+# obj#instance_variable_get(symbol) → obj
+# Returns the value of the given instance variable, or nil if the instance variable is not set.
+#
+# obj#instance_variable_set(symbol, obj)
+# Sets the instance variable names by symbol to object
+
+# self.name=('david')
