bceb77df374672bb45bf95c32cd0991d0eca7396
Mark Azemoon <mmazemoon@gmail.com>
2015-05-08 17:03:36 -0700

    Design Searchable module which adds the ability to search using ::where

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -2,11 +2,21 @@ require_relative 'db_connection'
 require_relative '01_sql_object'
 
 module Searchable
-  def where(params)
-    # ...
+  def where(params = {})
+    where, vals = [], []
+    params.each do |k, v|
+      where << "#{k} = ?"
+      vals << v
+    end
+    result = DBConnection.execute(<<-SQL, *vals)
+      SELECT *
+      FROM #{table_name}
+      WHERE #{where.join(" AND ")}
+    SQL
+    result.map{|obj| self.new(obj)}
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
